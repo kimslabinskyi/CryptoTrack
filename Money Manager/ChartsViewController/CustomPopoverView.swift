fbcfb0d1f9f7065c5acff2ddb3f5f6f234c2bb2b
Kim <kim.slabinskyi@gmail.com>
2024-08-21 19:37:24 +0200

    Create an algorithm to show values, improve UI

---
+++ Money Manager/ChartsViewController/CustomPopoverView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomPopoverView.swift
+//  Money Manager
+//
+//  Created by Kim on 13.08.2024.
+//
+
+import UIKit
+
+class CustomPopoverView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Money Manager/Network & DataSource/CryptoCurrencyType.swift
@@ -0,0 +1,33 @@
+//
+//  Cryptocurrency.swift
+//  Money Manager
+//
+//  Created by Kim on 18.08.2024.
+//
+
+import Foundation
+
+
+enum CryptoCurrencyType {
+    case btc
+    case eth
+    case bnb
+    case xrp
+    case ada
+    case sol
+    case dot
+    case doge
+    case ltc
+    case busd
+    
+    var url: String? {
+        switch self {
+        case .btc:
+            return "https://api.coingecko.com/api/v3/coins/bitcoin/market_chart"
+        case .eth:
+            return "https://api.coingecko.com/api/v3/coins/ethereum/market_chart"
+        default:
+             return nil
+        }
+    }
+}
