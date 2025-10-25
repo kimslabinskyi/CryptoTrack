f640fda8e252e606b76c92ef99c17cdb4af18202
Kim <kim.slabinskyi@gmail.com>
2025-10-25 08:10:38 +0200

    Update pods

--- Pods/Realm/Realm/RLMCollection.mm
+++ Pods/Realm/Realm/RLMCollection.mm
@@ -226,7 +226,7 @@ NSUInteger RLMUnmanagedFastEnumerate(id collection, NSFastEnumerationState *stat
 
     NSUInteger i = 0;
     for (id object in collection) {
-        copy->items[i++] = object;
+        copy->items.get()[i++] = object;
     }
 
     state->itemsPtr = (__unsafe_unretained id *)(void *)copy->items.get();
@@ -533,7 +533,8 @@ RLMNotificationToken *RLMAddNotificationBlock(id c, id block,
         if (!token->_realm) {
             return;
         }
-        RLMRealm *realm = token->_realm = [RLMRealm realmWithConfiguration:config queue:queue error:nil];
+        RLMRealm *realm = [RLMRealm realmWithConfiguration:config queue:queue error:nil];
+        token->_realm = realm;
         id collection = [realm resolveThreadSafeReference:tsr];
         token->_token = [collection addNotificationCallback:block keyPaths:info->keyPathArrayFromStringArray(keyPaths)];
     });
